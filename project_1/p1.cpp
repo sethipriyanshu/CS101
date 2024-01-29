39239199897715d8ccd268f1178251eb3853603c
Priyanshu <sethipriyanshu@outlook.com>
2024-01-29 17:35:25 -0600

    added search function and minimum function

--- project_1/p1.cpp
+++ project_1/p1.cpp
@@ -61,7 +61,110 @@ void ColFunction(string filename){
 }
 
 
+void SearchRowFunction(string filename, string col_name, string val, int columnWidth){
+    ifstream inFile;
+    inFile.open(filename);
+    if(!inFile){
+        cout << "Error: could not open file" << endl;
+        return;
+    }
+    string line;
+    getline(inFile, line);
+    stringstream ss(line);
+    string item;
+    int col_index = -1;
+    int i = 0;
+    if (col_name != "*") {
+        while (getline(ss, item, ',')) {
+            if (item == col_name) {
+                col_index = i;
+            }
+            cout << setw(columnWidth) << right << item;
+            i++;
+        }
+        cout << endl;
+        if (col_index == -1) {
+            cout << "Column not found" << endl;
+            return;
+        }
+    } else {
+        while (getline(ss, item, ',')) {
+            cout << setw(columnWidth) << right << item;
+        }
+        cout << endl;
+    }
+    bool found = false;
+    while(getline(inFile, line)){
+        stringstream ss(line);
+        string item;
+        int i = 0;
+        bool match = false;
+        while (getline(ss, item, ',')) {
+            if ((col_name == "*" && item == val) || (i == col_index && item == val)) {
+                match = true;
+                break;
+            }
+            i++;
+        }
+        if (match) {
+            found = true;
+            stringstream ss(line);
+            while (getline(ss, item, ',')) {
+                cout << setw(columnWidth) << right << item;
+            }
+            cout << endl;
+        }
+    }
+    if (!found) {
+        cout << "No results" << endl;
+    }
+    inFile.close();
+}
 
+
+void MinFunction(string filename, string col_name){
+    ifstream inFile;
+    inFile.open(filename);
+    if(!inFile){
+        cout << "Error: could not open file" << endl;
+        return;
+    }
+    string line;
+    getline(inFile, line);
+    stringstream ss(line);
+    string item;
+    int col_index = -1;
+    int i = 0;
+    while (getline(ss, item, ',')) {
+        if (item == col_name) {
+            col_index = i;
+        }
+        i++;
+    }
+    if (col_index == -1) {
+        cout << "Column not found" << endl;
+        return;
+    }
+    double min = 0;
+    bool first = true;
+    while(getline(inFile, line)){
+        stringstream ss(line);
+        string item;
+        int i = 0;
+        while (getline(ss, item, ',')) {
+            if (i == col_index) {
+                double val = stod(item);
+                if (first || val < min) {
+                    min = val;
+                    first = false;
+                }
+            }
+            i++;
+        }
+    }
+    cout<<"The min for col "<<col_name <<" = " << min << endl;
+    inFile.close();
+}
 
 int main(int argc, char* argv[]){
 
@@ -73,7 +176,7 @@ int main(int argc, char* argv[]){
     string userCmd = "";
     int columnWidth = stoi(argv[2]);
     string filename = argv[1];
-
+    MinFunction(filename,"Age");
     
     while(userCmd != "quit"){
         cout << "Enter a command or \"help\" for a command list:" << endl;
